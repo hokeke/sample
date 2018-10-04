3bac9c7a4b48f0d0ccd5c6f09dc1bbbb27ee5776
hokeke <30815088+hokeke@users.noreply.github.com>
2018-10-04 14:48:05 +0900

    Add files via upload

--- serviceworker.js
+++ serviceworker.js
@@ -1,25 +1,43 @@
+console.log("serviceworker");
+
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+
+var config = {
+  messagingSenderId: "27904652077"
+};
+firebase.initializeApp(config);
+
+var messaging = firebase.messaging();
+
 self.addEventListener('install', e => {
-    console.log('Service Worker oninstall: ', e);
-  });
-  
-  self.addEventListener('activate', e => {
-    console.log('Service Worker onactivate: ', e);
-  
-    e.waitUntil(self.clients.claim());
-  });
-  
-  self.addEventListener('fetch', e => {
-    console.log('Service Worker onfetch: ', e);
-  });
-  
-  self.addEventListener('push', e => {
-    console.log('Service Worker onpush: ', e);
-  
-    e.waitUntil(
-      self.registration.showNotification('Push Notification Title', {
-        body: 'message',
-        icon: 'http://placehold.it/192x192',
-        tag: 'push-notification-tag'
-      })
-    );
-  });
+  console.log('Service Worker oninstall: ', e);
+});
+
+self.addEventListener('activate', e => {
+  console.log('Service Worker onactivate: ', e);
+
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', e => {
+  console.log('Service Worker onfetch: ', e);
+});
+
+// self.addEventListener('push', e => {
+//   console.log('Service Worker onpush: ', e);
+
+//   e.waitUntil(
+//     self.registration.showNotification('Push Notification Title', {
+//       body: 'message',
+//       icon: 'http://placehold.it/192x192',
+//       tag: 'push-notification-tag'
+//     })
+//   );
+// });
+
+messaging.setBackgroundMessageHandler(function (e) {
+  console.log("onMessage", e);
+});
+
+console.log("finish serviceworker");
